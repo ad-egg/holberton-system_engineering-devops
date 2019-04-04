3a7db213d5835714a4d82c8f961a5444f5b27094
Athena Deng <539@holbertonschool.com>
2019-04-04 23:02:45 +0000

    made Puppet manifest conform to puppet-lint style guide

--- 0x0D-configuration_management/0-create_a_file.pp
+++ 0x0D-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # creates a file called holberton in /tmp directory
 file { '/tmp/holberton':
-  path    => '/tmp/holberton',
   ensure  => file,
+  path    => '/tmp/holberton',
   content => 'I love Puppet',
   group   => 'www-data',
   mode    => '0744',
