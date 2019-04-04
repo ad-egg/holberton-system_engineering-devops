d50df19e2919c809d68d6268d326e1d43ceac4eb
Athena Deng <539@holbertonschool.com>
2019-04-04 22:51:24 +0000

    first commit

---
+++ 0x0D-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# reates a file called holberton in /tmp directory
+file { '/tmp/holberton':
+  path    => '/tmp/holberton',
+  ensure  => file,
+  content => 'I love Puppet',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+}
