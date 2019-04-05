17e5946e1aa72e23f2746fb73b095513db5be5f0
Athena Deng <539@holbertonschool.com>
2019-04-05 00:59:18 +0000

    2-execute_a_command.pp

--- 0x0D-configuration_management/2-execute_a_command.pp
+++ 0x0D-configuration_management/2-execute_a_command.pp
@@ -1,3 +1,5 @@
 # a Puppet manifest that uses the exec resource to pkill a process killmenow
-exec { 'pkill killmenow':
+exec { 'pkill -f killmenow':
+  command => 'pkill -f killmenow',
+  path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ]
 }
