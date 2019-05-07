f8ef1d265b5ec8ecf2cb9b743358674a265e58d7
Athena Deng <539@holbertonschool.com>
2019-05-07 00:53:13 +0000

    first commit

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+uses REST API to get information on an employee's TODO list progress and
+exports the information in CSV format
+"""
+
+if __name__ == "__main__":
+    import csv
+    import requests
+    from sys import argv
+
+    employee_id = argv[1]
+
+    todos_url = "https://jsonplaceholder.typicode.com/todos"
+    users_url = "https://jsonplaceholder.typicode.com/users"
+
+    r_todos = requests.get(todos_url, params={'userId': employee_id})
+    r_users = requests.get(users_url, params={'id': employee_id})
+
+    total_tasks = r_todos.json()
+    user_info = r_users.json()
+    employee_name = user_info[0].get('name')
+
+    with open("{}.csv".format(employee_id), mode='w') as csv_file:
+        writer = csv.writer(csv_file, delimiter=',',quotechar='"', quoting=csv.QUOTE_ALL)
+        for task in total_tasks:
+            writer.writerow(["{}".format(employee_id),"{}".format(employee_name),"{}".format(task.get('completed')),"{}".format(task.get('title'))])
