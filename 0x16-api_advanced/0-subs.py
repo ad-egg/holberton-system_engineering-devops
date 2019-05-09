00c129ed962adac0217c5a80cb7ef61ff3f6374e
Athena Deng <539@holbertonschool.com>
2019-05-09 22:10:09 +0000

    first commit, incomplete work

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+this module contains a function that queries the Reddit API and returns the
+number of subscribers for a given subreddit, if an invalid subreddit is
+given, the function should return 0
+"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    queries the Reddit API and returns number of subscribers for a given
+    subreddit
+    """
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    r = requests.get(url, allow_redirects=False)
+
+    if r.status_code == 200:
+        req = r.json()
+        info = req.get('data')
+        subscribers = info.get('subscribers')
+        if info is not None and subscribers is not None:
+            return subscribers
+    return 0
