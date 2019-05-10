4dd26199d4a991cd264075eea5111e2d4faac1d3
Athena Deng <539@holbertonschool.com>
2019-05-10 03:36:14 +0000

    incomplete work

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+this module contains a function that queries the Reddit API and prints the
+titles of the first 10 hot posts listed for a given subreddit
+"""
+
+import requests
+
+def top_ten(subreddit):
+    """
+    function queries the Reddit API and prints the titles of the first 10 hot
+    posts listed for a given subreddit
+    """
+
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    h = {'User-agent': 'ad-egg'}
+    r = requests.get(url, headers=h, allow_redirects=False, params={'limit': 10})
+
+    if r.status_code == 200:
+        req = r.json()
+        data = req.get('data')
+        children = data.get('children')
