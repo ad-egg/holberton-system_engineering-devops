cc797322453e4f6fa3a6827ade754c571eedeb1d
Athena Deng <539@holbertonschool.com>
2019-05-12 03:35:50 +0000

    incomplete work

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+this module contains a recursive function that queries the Reddit API and
+returns a list containing the titles of all hot articles for a given subreddit
+"""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[]):
+    """
+    recursive function that queries the Reddit API and returns a list that
+    contains the titles of all hot articles for a given subreddit
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    h = {'User-agent': 'ad-egg'}
+    r = requests.get(url, headers=h, allow_redirects=False)
+
+    if req.status_code == 200:
+        req = req.json()
+        data = req.get('data')
+        children = data.get('children')
+        for post in children:
+            post_data = post.get('data')
+            title = post_data.get('title')
+            hot_list.append(title)
+    else:
+        return None
