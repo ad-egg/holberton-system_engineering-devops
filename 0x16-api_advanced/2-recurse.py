7766c653da2a2ec2816610ca8625aafc688ea0bf
Athena Deng <539@holbertonschool.com>
2019-05-16 02:00:25 +0000

    recursively adds titles of hot articles page by page

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -7,23 +7,27 @@ returns a list containing the titles of all hot articles for a given subreddit
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], after=''):
     """
     recursive function that queries the Reddit API and returns a list that
     contains the titles of all hot articles for a given subreddit
     """
-    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    url = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(subreddit, after)
     h = {'User-agent': 'ad-egg'}
     r = requests.get(url, headers=h, allow_redirects=False)
 
-    if req.status_code == 200:
-        req = req.json()
+    if r.status_code == 200:
+        req = r.json()
         data = req.get('data')
         children = data.get('children')
         for post in children:
             post_data = post.get('data')
             title = post_data.get('title')
             hot_list.append(title)
-        return hot_list
+        after = data.get('after')
+        if after is None:
+            return hot_list
+        else:
+            return recurse(subreddit, hot_list, after)
     else:
         return None
